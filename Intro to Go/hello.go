d57afe72c7e55bcdd31b53ef7e13be3523e316d9
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-07-30 21:02:49 -0600

    Create hello.go

---
+++ Intro to Go/hello.go
@@ -0,0 +1,10 @@
+package main
+// Every Go program must start with a package declaration. Packages are Go's way of organizing and reusing code. 
+// There are two types of Go programs: executables and libraries.
+
+import "fmt"
+// The import keyword is how we include code from other packages to use with our program.
+
+func main() {
+    fmt.Println("Hello World")
+}
